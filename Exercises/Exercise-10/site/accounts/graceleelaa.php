5b7c58b815a8537eec0661a3bb5df61473d0e578
graceleelaa <graceleelaa@Graces-Mac.local>
2017-04-27 16:44:00 -0700

    s

---
+++ Exercises/Exercise-10/site/accounts/graceleelaa.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: graceleelaa
+password: >
+  $2a$10$8fnmTNicTkCBfutAZfQzw.u/mapgd0kJjzzaBItJzDePCMgdH.OGO
+email: glee4@cca.edu
+language: en
+role: admin
+history:
+  - projects/project-a
+  - projects
+  - about
